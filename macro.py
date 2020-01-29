94abeb520854b19b06bbcceb717451516956a56a
Jiwon <poiu8944@gmail.com>
2020-01-30 04:04:00 +0900

    update
    mac에서 multiprocessing 기본은 spawn 이다
     : multiprocessing.set_start_method('spawn')
    
    + 데이터 가공

--- macro.py
+++ macro.py
@@ -9,7 +9,8 @@ from recognition import Recognition
 from pytesseract import *
 from PIL import Image, ImageGrab
 from keyboard import Keyboard
-from multiprocessing import Pool
+# from multiprocessing import Pool
+import multiprocessing
 import pyautogui as pag
 
 
@@ -28,7 +29,7 @@ def readFile():
     with open('positions.txt', 'r') as f:
         for line in f.readlines():
             if(line[0].isdigit()):
-                ret.append(line[:-1].split(' '))
+                ret.append(list(map(int, line[:-1].split(' '))))
 
     def chunker(seq, size):
         return list(seq[pos:pos + size] for pos in range(0, len(seq), size))
@@ -43,29 +44,45 @@ def click(x, y):
 
 
 def dbclick(arr):
+    # print("double click ")
+    # print(arr)
+    click(arr[0], arr[1])
     click(arr[0], arr[1])
 
 
+def init(param):
+    # 화면 setting #
+    # time.sleep(1)
+    window = param[0]
+    pos = param[1:]
+    dbclick(pos[0][window])  # 수강신청 들어가기
+    time.sleep(3)
+    dbclick(pos[1][window])  # 안내문 ok
+    time.sleep(1)
+    dbclick(pos[2][window])  # 왼쪽 창 접기
+    time.sleep(1)
+
+
 def initWindow(param):
-    def run(window):
-        # 화면 setting #
-        time.sleep(1)
-        dbclick(param[0][window])  # 수강신청 들어가기
-        time.sleep(3)
-        dbclick(param[1][window])  # 안내문 ok
-        time.sleep(1)
-        dbclick(param[2][window])  # 왼쪽 창 접기
-        time.sleep(1)
-    pool = Pool(processes=8)
-    pool.map(run, range(4))
+    multiprocessing.set_start_method('spawn')
+    arr = list([i] + param[:3] for i in range(4))
+    with multiprocessing.Pool(processes=8) as pool:
+        pool.map(init, arr)
+
+
+def macro(window):
+    window = param[0]
+    pos = param[1:]
+    num = getNumber(pos[1][window])
+    dbclick(pos[0][window])  # 확정하기
+    keyboard.Type(num + '\n')
 
 
 def run(param):
-    def macro(window):  # x : index
-        dbclick(param[7][window])  # 확정하기
-        keyboard.Type(getNumber(param[6][window]) + '\n')
-    pool = Pool(processes=8)
-    pool.map(macro, range(4))
+    multiprocessing.set_start_method('spawn')
+    arr = list([i] + param[6:] for i in range(4))
+    with multiprocessing.Pool(processes=8) as pool:
+        pool.map(macro, arr)
 
 
 http = urllib3.PoolManager()
@@ -88,15 +105,15 @@ print(gametime)
 if __name__ == '__main__':
     pos = readFile()
     initWindow(pos)
-    while True:
-        os.system('clear')
-        response = http.request('GET', url)
-        date = response.headers['Date']
-        date = datetime.datetime.strptime(date, '%a, %d %b %Y %H:%M:%S %Z')
-        flag = gametime - date < datetime.timedelta(seconds=2)  # 1초 전 true
-        print(date)
-        if flag:
-            time.sleep(0.4)  # 0.4초 뒤에 실행
-            break
-        time.sleep(timegap)
-    run(pos)
+while True:
+    os.system('clear')
+    response = http.request('GET', url)
+    date = response.headers['Date']
+    date = datetime.datetime.strptime(date, '%a, %d %b %Y %H:%M:%S %Z')
+    flag = gametime - date < datetime.timedelta(seconds=2)  # 1초 전 true
+    print(date)
+    if flag:
+        time.sleep(0.4)  # 0.4초 뒤에 실행
+        break
+    time.sleep(timegap)
+run(pos)
